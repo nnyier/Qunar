9c690a98222e5694b878a37b6260075b6f38cfa8
nnyier <1049194608@qq.com>
2018-07-05 14:34:06 +0800

    city-search

--- src/components/city/city.vue
+++ src/components/city/city.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="city">
-        <city-header></city-header>
-    </div>
+  <div class="city">
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 
 <script>
 import CityHeader from "../city/citypages/header";
+import CitySearch from "../city/citypages/search";
 
 export default {
   name: "City",
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 };
 </script>
---
+++ src/components/city/citypages/search.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="city-search">
+        <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+</template>
+
+<script>
+export default {
+  name: "CitySearch"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '../../../common/css/variables.styl';
+
+.city-search {
+    height: 0.72rem;
+    background: $bg-color;
+    padding: 0 0.1rem;
+
+    .search-input {
+        box-sizing: border-box;
+        height: 0.62rem;
+        line-height: 0.62rem;
+        width: 100%;
+        text-align: center;
+        border-radius: 0.06rem;
+        padding: 0 0.1rem;
+        font-size: 0.2rem;
+        color: #666;
+    }
+}
+</style>

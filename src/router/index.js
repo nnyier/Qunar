5775a9c9f0cff5f76c39299fe410dd749e6012f3
nnyier <1049194608@qq.com>
2018-07-26 11:22:15 +0800

    project finish

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Home from '@/components/home/home'
-// import City from '../components/city/city'
-// import InHome from '../components/city/citypages/inhome'
-// import OutHome from '../components/city/citypages/outhome'
-// import Detail from '../components/detail/detail'
+import Home from '@/components/home/home'
+import City from '../components/city/city'
+import InHome from '../components/city/citypages/inhome'
+import OutHome from '../components/city/citypages/outhome'
+import Detail from '../components/detail/detail'
 
 Vue.use(Router)
+
 export default new Router({
-  // 异步加载组件
   routes: [{
     path: '/',
-    component: () => import('@/components/home/home')
+    component: Home
   }, {
     path: '/city',
-    component: () => import('../components/city/city'),
+    component: City,
     children: [{
       path: '/',
       redirect: 'inhome'
     },
     {
       path: 'inhome',
-      component: () => import('../components/city/citypages/inhome')
+      component: InHome
     },
     {
       path: 'outhome',
-      component: () => import('../components/city/citypages/outhome')
+      component: OutHome
     }]
   }, {
     path: '/detail/:id',
-    component: () => import('../components/detail/detail')
+    component: Detail
   }],
   // 每次切换路由，都让页面滚动到顶部
   scrollBehavior (to, from, savedPosition) {
